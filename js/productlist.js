bfcbad3ce789ad37612e880b968322c0f7b950f7
Jonas Holbech <jh@jonasholbech.dk>
2023-01-27 15:27:54 +0100

    hæsblæsende tur

---
+++ js/productlist.js
@@ -0,0 +1,55 @@
+fetch("https://kea-alt-del.dk/t7/api/products")
+  .then((res) => res.json())
+  .then(showProducts);
+
+function showProducts(products) {
+  //looper og kalder showProduct
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  //console.log(product);
+  //fang template
+  const template = document.querySelector("#smallProductTemplate").content;
+  //lav en kopi
+  const copy = template.cloneNode(true);
+  //ændre indhold
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  if (product.soldout) {
+    //produktet er udsolgt
+    copy.querySelector("article").classList.add("soldOut");
+  }
+  //appende
+  document.querySelector("main").appendChild(copy);
+}
+/*
+<article class="smallProduct">
+          <img
+            src="https://kea-alt-del.dk/t7/images/webp/640/1163.webp"
+            alt="Sahara Team India Fanwear Round Neck Jersey"
+          />
+          <h3>Sahara Team India Fanwear Round Neck Jersey</h3>
+          <p class="subtle">Tshirts | Nike</p>
+          <p class="price"><span>Prev.</span> DKK 1595,-</p>
+          <div class="discounted">
+            <p>Now DKK 1560,-</p>
+            <p>-34%</p>
+          </div>
+          <a href="product.html">Read More</a>
+        </article>
+{
+  "id": 1165,
+  "gender": "Men",
+  "category": "Apparel",
+  "subcategory": "Topwear",
+  "articletype": "Tshirts",
+  "season": "Summer",
+  "productionyear": 2013,
+  "usagetype": "Sports",
+  "productdisplayname": "Mean Team India Cricket Jersey",
+  "price": 2495,
+  "discount": 45,
+  "brandname": "Nike",
+  "soldout": 0
+}
+*/
